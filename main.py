8a91731db7da207c56420142eecba74347c3e782
MaeLPL <balzack@tlen.pl>
2022-02-02 20:37:33 +0100

    v1

--- main.py
+++ main.py
@@ -17,7 +17,53 @@ pygame.mixer.init(22100, -16, 2, 32)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+def draw_game():
+
+    # surface
+    SURFACE_MAIN.fill((255,255,255))
+    pygame.display.flip()
+
+
+def game_main_loop():
+    events_list = pygame.event.get()
+    game_quit = False
+
+    # player_action = "no-action"
+
+    while not game_quit:
+        # player action definition
+
+        # handle player input
+
+
+
+        ### process INPUT
+
+        draw_game()
+        for x in range(5):
+            print (x+1)
+        for event in events_list:
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_c:
+                    game_quit = True
+
+    pygame.quit()
+    exit()
+
+
+def game_initialize():
+    global SURFACE_MAIN
+    pygame.init()
+
+    pygame.key.set_repeat(500,70) # key repeat function (after ms, repeat every ms)
+
+    SURFACE_MAIN = pygame.display.set_mode((800,600))
+
 if __name__ == '__main__':
 
-    print('test print message')
+
+    game_initialize()
+
     pygame.display.set_caption('MaeLs dank dung dung')
+    game_main_loop()
