289f30618c0d0aed4d86ddb4db8774c3a6d69595
MaeLPL <balzack@tlen.pl>
2022-02-02 20:08:51 +0100

    v1

--- main.py
+++ main.py
@@ -2,15 +2,22 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import tcod
+import os, sys
+import random, time
+import tcod.map
 
+### for Imaging
+import numpy as np
+from PIL import Image
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-    print('test')
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharmttt')
+### pygame extras
+pygame.mixer.init(22100, -16, 2, 32)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+
+    print('test print message')
+    pygame.display.set_caption('MaeLs dank dung dung')
